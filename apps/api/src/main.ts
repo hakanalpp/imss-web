c08f7132e2e2f2986dc43662bf4c6d97fe00ae5f
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-04-09 15:25:56 +0300

    feat: api prefix deleted

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,7 +10,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = "api";
+  const globalPrefix = "";
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(
     new ValidationPipe({
